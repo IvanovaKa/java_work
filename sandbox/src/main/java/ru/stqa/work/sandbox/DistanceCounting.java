d42589aecd40e9bd47140f17c3d9240345e87106
IvanovaKa <ivanovaka83@gmail.com>
2017-09-19 21:30:00 +0100

    Рассчет расстояния между двумя точками

---
+++ sandbox/src/main/java/ru/stqa/work/sandbox/DistanceCounting.java
@@ -0,0 +1,17 @@
+package ru.stqa.work.sandbox;
+
+public class DistanceCounting {
+
+  public static void main(String[] args) {
+
+   Point p1 = new Point(5,4);
+
+
+   Point p2 = new Point(1,1);
+
+    System.out.println("Расстояние между двумя точками p1 и p2 = " + p1.distance(p2));
+
+  }
+
+
+}
