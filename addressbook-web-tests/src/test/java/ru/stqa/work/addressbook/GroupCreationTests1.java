1fac7e2d497d1f951fdc25005e6b94ad36797522
IvanovaKa <ivanovaka83@gmail.com>
2017-09-21 16:15:55 +0100

    Переименование  тестового метода и изменение метода инициализации фикстуры

--- addressbook-web-tests/src/test/java/ru/stqa/work/addressbook/GroupCreationTests1.java
+++ addressbook-web-tests/src/test/java/ru/stqa/work/addressbook/GroupCreationTests1.java
@@ -22,10 +22,6 @@ public class GroupCreationTests1 {
     public void setUp() throws Exception {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationTests1() {
         wd.get("http://localhost:8080/addressbook/addressbook/edit.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -37,6 +33,11 @@ public class GroupCreationTests1 {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+
+    }
+    
+    @Test
+    public void testGroupCreation() {
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
