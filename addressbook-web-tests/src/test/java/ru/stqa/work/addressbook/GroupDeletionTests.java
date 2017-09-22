267b629a5d6ff5974459c6a4c86f341d184e208d
IvanovaKa <ivanovaka83@gmail.com>
2017-09-22 09:08:35 +0100

    Создание общего базового класса TestBase для тестов
    перенесены вспомогательные методы в новый класс

---
+++ addressbook-web-tests/src/test/java/ru/stqa/work/addressbook/GroupDeletionTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.work.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase{
+
+    @Test
+    public void testGroupDeletion() {
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroup();
+        returnToGroupPage();
+    }
+
+}
