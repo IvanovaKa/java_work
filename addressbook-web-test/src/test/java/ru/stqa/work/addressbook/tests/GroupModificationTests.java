90befcea50307d57b9109f9a10663e33e57dbc73
IvanovaKa <ivanovaka83@gmail.com>
2017-09-25 16:40:06 +0100

    Создан GroupModificationTests класс

---
+++ addressbook-web-test/src/test/java/ru/stqa/work/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.work.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.work.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("group name", "header", "footer"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
