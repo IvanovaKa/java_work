b97e1c409333015079895b00f4a3913f89666e4d
IvanovaKa <ivanovaka83@gmail.com>
2017-10-01 18:53:35 +0300

    Успешный запуск тестов в CHROME

--- addressbook-web-test/src/test/java/ru/stqa/work/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/work/addressbook/tests/TestBase.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @AfterMethod
   public void tearDown() {
