3b6e17c2b28085176d3e0e179ae2eefb36f4291d
IvanovaKa <ivanovaka83@gmail.com>
2017-10-07 19:15:13 +0300

    Primes
    Циклы: многократные повторения похожих действий

---
+++ sandbox/src/main/java/ru/stqa/work/sandbox/Primes.java
@@ -0,0 +1,61 @@
+package ru.stqa.work.sandbox;
+
+public class Primes {
+
+  public static boolean isPrime(int n) {
+    for (int i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean isPrimeFast(int n) {
+    for (int i = 2; i < n/2; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean isPrimeFaster(int n) {
+    int m = (int)Math.sqrt(n);
+    for (int i = 2; i < m; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean isPrimeWhie(int n) {
+    int i = 2;
+    while (i < n) {
+      if (n % i == 0) {
+        return false;
+      }
+      i++;
+    }
+    return true;
+  }
+
+  // упрощаем while
+  public static boolean isPrimeWhieEasy(int n) {
+    int i = 2;
+    while (i < n && n % i != 0) {
+      i++;
+    }
+    return i == n;
+  }
+
+  public static boolean isPrime(long n) {
+    for (long i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
