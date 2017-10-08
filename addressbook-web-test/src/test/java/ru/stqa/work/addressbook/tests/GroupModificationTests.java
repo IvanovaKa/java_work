5614f736332564e89a01c5729e188301eb67dc90
IvanovaKa <ivanovaka83@gmail.com>
2017-10-08 19:13:09 +0300

    Сравнение списков для GroupModificationTests
    4.7. Множества: неупорядоченные коллекции

--- addressbook-web-test/src/test/java/ru/stqa/work/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/work/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.work.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -17,12 +18,15 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() -1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("group name", "header", "footer"));
+    GroupData group = new GroupData(before.get(before.size() -1).getId(), "group name", "header", "footer");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
-//"group name", null, null)
-//"group name", "header", "footer"
