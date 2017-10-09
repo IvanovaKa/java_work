d5fd182d331c52166817dca7648457f137748c72
IvanovaKa <ivanovaka83@gmail.com>
2017-10-09 18:31:33 +0300

    Вычисление максимального идентификатора с использованием лямбда-выражения для ContactCreationTests

--- addressbook-web-test/src/test/java/ru/stqa/work/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/work/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.work.addressbook.model.ContactData;
 import ru.stqa.work.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getNavigationHelper().goToAddNewPage();
-    ContactData contact = new ContactData("Name", "Middle Name",
+    ContactData contact = new ContactData("Name2", "Middle Name",
             "Last Name", "Nickname", "Title", "Company", "Address",
             "1111111", "2222222", "3333333", "4444444",
             "email@email.com", "homepage", "1983", "1983",
@@ -23,16 +24,8 @@ public class ContactCreationTests extends TestBase {
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
-    contact.setId(max);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
-
 }
