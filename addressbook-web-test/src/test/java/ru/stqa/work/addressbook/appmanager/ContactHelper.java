e556177184777895e9d4067c0a4b5f13a4b8e74a
IvanovaKa <ivanovaka83@gmail.com>
2017-10-09 14:47:55 +0300

    Сравниваем размер списка для Contacts - fix
    (исправлена ошибка с локатором + добавлена фамилия)

--- addressbook-web-test/src/test/java/ru/stqa/work/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-test/src/test/java/ru/stqa/work/addressbook/appmanager/ContactHelper.java
@@ -122,10 +122,11 @@ public class ContactHelper extends HelperBase {
 
   public List<ContactData> getContactList() {
   List<ContactData> contacts = new ArrayList<ContactData>();
-  List<WebElement> elements = wd.findElements(By.name("selected[]"));
+  List<WebElement> elements = wd.findElements(By.name("entry"));
   for (WebElement element : elements) {
-    String name = element.getText();
-    ContactData contact = new ContactData(name, null, null, null, null,
+    String firstName = element.findElement(By.cssSelector("td:nth-child(3)")).getText();
+    String lastName = element.findElement(By.cssSelector("td:nth-child(2)")).getText();
+    ContactData contact = new ContactData(firstName, null, lastName, null, null,
             null, null, null, null, null, null,
             null, null, null, null, null, null,
             null, null);
