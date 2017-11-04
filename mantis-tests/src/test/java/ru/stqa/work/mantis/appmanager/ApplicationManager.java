54a4b3e95b4aa49fe90ebf6e7ad2a78b6b0e3185
IvanovaKa <ivanovaka83@gmail.com>
2017-11-04 08:10:27 +0200

    Промежуточные настройки

--- mantis-tests/src/test/java/ru/stqa/work/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/work/mantis/appmanager/ApplicationManager.java
@@ -1,5 +1,4 @@
 package ru.stqa.work.mantis.appmanager;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,14 +12,13 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-//import sun.plugin2.util.BrowserType;
-
 public class ApplicationManager {
 
   private final Properties properties;
-  WebDriver wd;
+  private WebDriver wd;
 
   private String browser;
+  private RegistrationHelper registrationHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -30,25 +28,41 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    }
-
-    //можно убирать 60 - для быстрого прохождения теста, но если необходимо время на появление элемента - нужно будет вернуть
-    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
-   }
-
-  public void stop() {
-    wd.quit();
   }
 
-  public  HttpSession newSession()  {
-    return  new HttpSession(this);
+  public void stop() {
+    if (wd != null){
+      wd.quit();
+    }
+  }
+
+  public HttpSession newSession() {
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null){
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+    if(wd == null){
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
   }
 }
