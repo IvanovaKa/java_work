6dea0d3bab82c4721a388bce74e81a7b421013db
IvanovaKa <ivanovaka83@gmail.com>
2017-11-05 13:43:12 +0200

    Получение почты на встроенный в тесты почтовый сервер
    8.6. Получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/out/test/resources/config_inc.php
+++ mantis-tests/out/test/resources/config_inc.php
@@ -1,7 +1,10 @@
 <?php
 $g_hostname = 'localhost';
-$g_db_type = 'mysql';
+$g_db_type = 'mysqli';
 $g_database_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
-$g_singup_use_captcha = OFF;
+$g_signup_use_captcha = OFF;
+$d_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
+
