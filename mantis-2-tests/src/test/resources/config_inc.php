6cfc7f6f63feaecf435f6862601b254843cbbffa
IvanovaKa <ivanovaka83@gmail.com>
2017-11-05 16:06:52 +0200

    Реализовано получение почты на встроенный в тесты почтовый сервер
    8.6. Получение почты на встроенный в тесты почтовый сервер

--- mantis-2-tests/src/test/resources/config_inc.php
+++ mantis-2-tests/src/test/resources/config_inc.php
@@ -7,3 +7,5 @@ $g_db_password = '';
 $g_signup_use_captcha = OFF;
 $g_default_timezone       = 'Europe/Berlin';
 $g_crypto_master_salt     = 'HJhOwiSPwl058idaqE/1SzUQndU0mos1i1NWgmQ+tXU=';
+$d_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
